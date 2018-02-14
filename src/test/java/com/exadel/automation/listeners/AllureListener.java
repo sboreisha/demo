4cf0d462cbd942e6661d29eb59f53c5dda5f5f98
Sergei Boreisha <sboreisha@exadel.com>
2018-02-14 15:12:51 +0300

    initial

--- src/test/java/com/exadel/automation/listeners/AllureListener.java
+++ src/test/java/com/exadel/automation/listeners/AllureListener.java
@@ -1,17 +1,10 @@
 package com.exadel.automation.listeners;
 
-import com.exadel.automation.TestBase;
-import io.qameta.allure.Attachment;
-import org.apache.commons.lang.StringUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
 
 public class AllureListener extends TestListenerAdapter {
 
-    @Override
+    /*@Override
     public void onTestFailure(ITestResult result) {
         if (result.getMethod().getGroups().length == 0) {
             Object currentClass = result.getInstance();
@@ -25,5 +18,5 @@ public class AllureListener extends TestListenerAdapter {
     @Attachment(value = "Failed test screenshot", type = "image/png")
     private byte[] saveScreenshot(byte[] screenshot) {
         return screenshot;
-    }
+    }*/
 }
